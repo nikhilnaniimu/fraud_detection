a7a1c83285b797c30b6d695f29d1d76a2f586059
nikhilnaniimu <nikhil.2019ex@iimu.ac.in>
2022-04-30 15:40:04 +0530

    added 3 new model code

---
+++ ml_model.py
@@ -0,0 +1,14 @@
+
+
+print("adding all ml related code here")
+
+
+def model1():
+    print("adding code for ml model 1")
+
+def model2():
+    print("adding code for ml model 2")
+
+
+def model3():
+    print("adding code for ml model 3")
